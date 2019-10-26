4879874813a10b00a9a66fc3db3d0dd0b3d62b03
Aditya Jain <contact.howsthatapp@gmail.com>
2019-10-26 12:57:36 +0530

    2

--- app/src/main/java/com/example/studentpro/MainActivity.java
+++ app/src/main/java/com/example/studentpro/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.studentpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signin,signup;
+
+        signin = findViewById(R.id.signin);
+        signup = findViewById(R.id.signup);
+       TextView user = (TextView) findViewById(R.id.user);
+        TextView pass = (TextView) findViewById(R.id.pass);
+
+      String user_str = user.getText().toString();
+      String pass_str = pass.getText().toString();
+
+
+
+
+// Sign in
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+
+            }
+        });
+
+
+        // Sign UP
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , SignUp.class));
+
+
+            }
+        });
+
+
+
+
     }
 }
