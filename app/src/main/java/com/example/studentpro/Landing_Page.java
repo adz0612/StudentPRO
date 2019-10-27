aae37a8040dfec6136ddfea53745814cef4b8389
Aditya Jain <contact.howsthatapp@gmail.com>
2019-10-27 06:53:14 +0530

    5

--- app/src/main/java/com/example/studentpro/Landing_Page.java
+++ app/src/main/java/com/example/studentpro/Landing_Page.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -11,18 +12,25 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import java.sql.BatchUpdateException;
 
 public class Landing_Page extends AppCompatActivity {
+     float i=0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing__page);
-        int i =0;
-        TextView scrl2,scrl3,scrl4,scrl5,scrl6,scrl7,scrl8,scrl9;
-        Button scrl1;
-        scrl1 = findViewById(R.id.textView90);
+
+        final TextView scrl2,scrl3,scrl4,scrl5,scrl6,scrl7,scrl8,scrl9,scrl10,progress_123;
+        progress_123 = findViewById(R.id.Progress_123);
+
+        final Button scrl1;
+         scrl1 = findViewById(R.id.textView90);
         scrl2 = findViewById(R.id.textView91);
         scrl3 = findViewById(R.id.textView92);
         scrl4 = findViewById(R.id.textView93);
@@ -31,6 +39,10 @@ public class Landing_Page extends AppCompatActivity {
         scrl7 = findViewById(R.id.textView96);
         scrl8 = findViewById(R.id.textView97);
         scrl9 = findViewById(R.id.textView98);
+        scrl10 = findViewById(R.id.textView99);
+
+
+
 
 
 
@@ -40,22 +52,148 @@ public class Landing_Page extends AppCompatActivity {
         scrl1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int i =1;
+
 
                 i++;
-               int a = (i/9)*100;
 
 
+                float a = (i/10)*100;
 
-                ProgressBar pb;
-                pb = findViewById(R.id.progressBar);
-                pb.setProgress(a);
+                progress_123.setText(""+a+"%/100%");
+
+
+scrl1.setEnabled(false);
+scrl1.setText("Completed");
 
             }
         });
 
 
 
+        scrl2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                scrl2.setEnabled(false);
+
+                scrl2.setText("Completed");
+
+i++;
+                float a = (i/10)*100;
+                progress_123.setText(""+a+"%/100%");
+
+            }
+        });
+        scrl3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrl3.setText("Completed");
+
+                scrl3.setEnabled(false);
+                i++;
+
+                float a = (i/10)*100;
+                progress_123.setText(""+a+"%/100%");
+
+            }
+        });
+        scrl4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrl4.setText("Completed");
+
+                scrl4.setEnabled(false);
+                i++;
+
+                float a = (i/10)*100;
+                progress_123.setText(""+a+"%/100%");
+
+            }
+        });
+        scrl5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrl5.setEnabled(false);
+                scrl5.setText("Completed");
+
+                i++;
+
+                float a = (i/10)*100;
+                progress_123.setText(""+a+"%/100%");
+
+            }
+        });
+        scrl6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrl6.setEnabled(false);
+                scrl6.setText("Completed");
+
+                i++;
+
+                float a = (i/10)*100;
+                progress_123.setText(""+a+"%/100%");
+
+            }
+        });
+        scrl7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrl7.setEnabled(false);
+                scrl7.setText("Completed");
+
+                i++;
+
+                float a = (i/10)*100;
+                progress_123.setText(""+a+"%/100%");
+
+            }
+        });
+        scrl8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrl8.setEnabled(false);
+                scrl8.setText("Completed");
+
+                i++;
+
+                float a = (i/10)*100;
+                progress_123.setText(""+a+"%/100%");
+
+            }
+        });
+        scrl9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrl9.setEnabled(false);
+                scrl9.setText("Completed");
+
+                i++;
+
+                float a = (i/10)*100;
+                progress_123.setText(""+a+"%/100%");
+
+            }
+        });
+
+
+     scrl10.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+
+             scrl10.setEnabled(false);
+             scrl10.setText("Completed");
+
+             i++;
+
+             float a = (i/10)*100;
+             progress_123.setText(""+a+"%/100%");
+
+
+
+
+         }
+     });
 
 
 
@@ -79,8 +217,7 @@ public class Landing_Page extends AppCompatActivity {
 
 
 
-
-Button proff, voucher , other;
+    Button proff, voucher , other;
 
 proff = findViewById(R.id.proff);
         voucher = findViewById(R.id.vouch);
--- app/src/main/java/com/example/studentpro/Other.java
+++ app/src/main/java/com/example/studentpro/Other.java
@@ -2,13 +2,65 @@ package com.example.studentpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
 
 public class Other extends AppCompatActivity {
-
+private WebView mapps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+
+        Button maps,other;
+
+        maps = findViewById(R.id.button6);
+        other = findViewById(R.id.button7);
+
+
+        mapps = findViewById(R.id.mappss);
+
+
+
+        maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                WebSettings webSettings = mapps.getSettings();
+                webSettings.setJavaScriptEnabled(true);
+                mapps.setWebViewClient(new WebViewClient());
+                mapps.loadUrl("https://www.google.com/maps/search/men+pg+near+me/@12.9300679,77.5275457,15z/data=!3m1!4b1");
+
+
+
+
+
+
+            }
+        });
+
+        other.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WebSettings webSettings = mapps.getSettings();
+                webSettings.setJavaScriptEnabled(true);
+                mapps.setWebViewClient(new WebViewClient());
+mapps.loadUrl("https://www.google.com/maps/search/police+stations+near+me/@12.9300651,77.5012809,13z/data=!3m1!4b1");
+
+
+            }
+        });
+
+
+
     }
 }
